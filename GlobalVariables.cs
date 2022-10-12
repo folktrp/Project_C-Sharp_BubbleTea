118b9175610cf9e1800ab5685feff372d4adb88e
folktrp <71695556+folktrp@users.noreply.github.com>
2022-10-12 21:49:37 +0700

    Add files via upload

---
+++ GlobalVariables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPBubbleTea
+{
+    public class GlobalVariables
+    {
+
+        public static string APP_NAME = ConfigurationManager.AppSettings["AppName"];
+        public static string VERSION = ConfigurationManager.AppSettings["Version"];
+        public static string USER = string.Empty;
+        public static string DEV_BY = ConfigurationManager.AppSettings["DevBy"];
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using FTPBubbleTea.Forms;
+using FTPBubbleTea.Forms.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FTPBubbleTea
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            //ถ้า Login สำเร็จจะเปิดหน้า frmMain
+            frmLogin frm = new frmLogin();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new frmMain());  
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
